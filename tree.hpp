8745a8354647c6e307a0d6b65fbda5bf86538d4f
Klism (josh) <81436003+yourlocaljosh@users.noreply.github.com>
2025-01-06 22:44:02 -0500

    rotate right&left added

--- tree.hpp
+++ tree.hpp
@@ -32,6 +32,31 @@ class Tree {
             }
             return 0;
         }
+
+        Node* rotateRight(Node* y) {
+            Node* x = y->left;
+            Node* T2 = x->right;
+
+            x->right = y;
+            y->left = T2;
+
+            y->height = std::max(height(y->left), height(y->right)) + 1;
+            x->height = std::max(height(x->left), height(x->right)) + 1;
+
+            return x;
+        }
+
+         Node* rotateLeft(Node* x) {
+            Node* y = x->right;
+            Node* T2 = y->left;
+
+            y->left = x;
+            x->right = T2;
+
+            x->height = std::max(height(x->left), height(x->right)) + 1;
+            y->height = std::max(height(y->left), height(y->right)) + 1;
+            return y;
+        }
 };
 
 #endif
